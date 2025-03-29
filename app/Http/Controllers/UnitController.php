c623e9148976530edaf28263ebfca8329b52b57c
raphaelrychard <raph.rych@gmail.com>
2025-03-29 12:55:37 -0400

    feat(unit): adiciona request de atualização e ajustes no controller
    - Cria `UpdateUnitRequest.php` para validação de atualização de unidade
    - Ajusta `StoreUnitRequest.php` com melhorias na validação
    - Modifica `UnitController.php` para suportar atualização de unidade

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -5,15 +5,38 @@ declare(strict_types = 1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUnitRequest;
+use App\Http\Requests\UpdateUnitRequest;
 use App\Http\Resources\UnitResource;
 use App\Models\Unit;
 
 class UnitController extends Controller
 {
+    public function index()
+    {
+        return UnitResource::collection(
+            Unit::all(),
+        );
+    }
+
+    public function show(Unit $unit)
+    {
+        return UnitResource::make($unit);
+    }
+
     public function store(StoreUnitRequest $request)
     {
         $data = $request->validated();
+
         $unit = Unit::create($data);
+
+        return UnitResource::make($unit);
+    }
+
+    public function update(UpdateUnitRequest $request, Unit $unit)
+    {
+        $data = $request->validated();
+
+        $unit->update($data);
 
         return UnitResource::make($unit);
     }
--- app/Http/Requests/UpdateUnitRequest.php
+++ app/Http/Requests/UpdateUnitRequest.php
@@ -6,11 +6,11 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUnitRequest extends FormRequest
+class UpdateUnitRequest extends FormRequest
 {
     /**
-      * Determine if the user is authorized to make this request.
-      */
+     * Determine if the user is authorized to make this request.
+     */
     public function authorize(): bool
     {
         return true;
